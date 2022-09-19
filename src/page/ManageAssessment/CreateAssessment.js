92fdc9d7326a677cf65087a6f7adcaa183e15f2e
YaiYaiYaiCS2 <yaisaengviai@gmail.com>
2022-09-19 10:28:13 +0700

    juk

--- src/page/ManageAssessment/CreateAssessment.js
+++ src/page/ManageAssessment/CreateAssessment.js
@@ -88,7 +88,7 @@ function CreateAssessment() {
                           
                           return(
                             <div className="box-inp-title-1_1-create-ass" key={i}>
-                              <span className='num-title-1_1-create-ass'>1-1.{i+1} </span>
+                              <span className='num-title-1_1-create-ass'>1.{i+1} </span>
                               <input className='inp-title-1_1-create-ass'
                                 type="text"
                                 value={data}
